a8ec1dab4a8ba004977941a2ff338152ea6174c2
Karen <karencontreras84@gmail.com>
2026-02-16 19:35:33 -0500

    My first commmit

---
+++ main.py
@@ -0,0 +1,10 @@
+def sumar(a, b):
+    resultado = a + b
+    print(f"El resultado de la suma es: {resultado}")
+    return resultado
+
+if __name__ == "__main__":
+    print("Antes de llamar a la función sumar()")
+    sumar(5, 3)
+    print("Después de llamar a la función sumar()")
+
